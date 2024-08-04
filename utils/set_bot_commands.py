ccb25d30f745266975dd293c5314228ae6a2a30c
jamshidsodiqov <sodiqovjamshid50@gmail.com>
2024-08-04 23:40:00 +0500

    set_bot_commands modified

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,7 +4,11 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("help", "Yordam"),
+            types.BotCommand("start", "Start the bot. After the command you can send excel file."),
+            types.BotCommand("help", "Guidance for using bot!\n"
+                                     " After /start command you need to send file,\n"
+                                     " because after the command bot will be in state,\n"
+                                     " it expect only file from user. \n"
+                                     "If you send text or other thing bot won't reply you."),
         ]
     )
